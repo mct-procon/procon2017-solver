86bdba108f4da418a9cec2c79e6f179820c5adbf
j1618 <j1618@matsue-ct.jp>
2017-07-06 22:49:58 +0900

    Added the PuzzleSolver Solution.

---
+++ PuzzleSolver/PuzzleSolver/DXLib/Geometory.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace DxLib {
+    public static partial class DX {
+    }
+}
---
+++ PuzzleSolver/PuzzleSolver/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxLib;
+
+namespace PuzzleSolver {
+    class Program {
+        [STAThread]
+        static void Main(string[] args) {
+            DX.ChangeWindowMode(true);
+            if (DX.DxLib_Init() == -1) Environment.Exit(-1);
+
+            while(DX.ProcessMessage() != -1) {
+
+            }
+            DX.DxLib_End();
+        }
+    }
+}
