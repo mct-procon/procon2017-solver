e24fd3139cd010949e06221ef4fa8671fd30a673
j1211 <j1211@matsue-ct.jp>
2017-08-11 23:48:08 +0900

    多角形の実装
    頂点数／面積／当たり判定／平行移動／回転／反転を実装しました。浮動小数点数誤差の対策もしてあります。
    
    一般的な当たり判定では、多角形p1が多角形p2を含むケースも考慮する必要があります。しかし今回は、当たり判定を使う場所が限定されている（ピースとピース,
    or, ピースと枠の2辺をくっつける場合だけを考える）ので、線分同士の接触だけ実装すれば十分です。

--- PuzzleSolver/PuzzleSolver/Poly.cs
+++ PuzzleSolver/PuzzleSolver/Poly.cs
@@ -8,7 +8,74 @@ namespace PuzzleSolver
 {
 	class Poly
 	{
-		List<Point> points;		//点の集合.   実体.
-		List<Line>  lines;		//線分の集合. 実体.
+		public List<Point> points { get; private set; }		//点列(始点 = 終点). 実体.
+		public List<Line> lines { get; private set; }       //線分の集合. 実体.
+		private bool isPiece;
+
+		//コンストラクタ
+		Poly() { }
+		Poly(List<Point> points, List<Line> lines, bool isPiece)
+		{
+			this.points  = points;
+			this.lines   = lines;
+			this.isPiece = isPiece; 
+		}
+
+		//多角形の頂点数
+		int Count { get { return points.Count - 1; } }
+
+		//面積
+		double Area
+		{
+			get
+			{
+				double area = 0;
+				for (int i = 0; i < Count; i++)
+				{
+					area += Point.Cross(points[i], points[i + 1]);
+				}
+				area /= 2;
+				return area;
+			}
+		}
+
+		//線分が接触しているか
+		bool isHitLine(Poly poly)
+		{
+			for (int i = 0; i < Count; i++)
+			{
+				Line line1 = new Line(points[i], points[i + 1]);
+				for (int j = 0; j < poly.Count; j++)
+				{
+					Line line2 = new Line(poly.points[i], poly.points[i + 1]);
+					if (Line.IsHit(line1, line2))
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		//平行移動
+		void Trans(Point t, bool isUpdateLines)
+		{
+			for (int i = 0; i < points.Count; i++) { points[i] += t; }
+			if (isUpdateLines) { for (int i = 0; i < lines.Count; i++) { lines[i].Trans(t); } }
+		}
+
+		//反転 (軸はy = 0)
+		void Reverse(bool isUpdateLines)
+		{
+			for (int i = 0; i < points.Count; i++) { points[i] = points[i].Conj; }
+			if (isUpdateLines) { for (int i = 0; i < lines.Count; i++) { lines[i].Reverse(); } }
+		}
+
+		//乗算 (回転)
+		void Mul (Point mulValue, bool isUpdateLines)
+		{
+			for (int i = 0; i < points.Count; i++) { points[i] = points[i] * mulValue; }
+			if (isUpdateLines) { for (int i = 0; i < lines.Count; i++) { lines[i].Mul(mulValue); } }
+		}
 	}
 }
