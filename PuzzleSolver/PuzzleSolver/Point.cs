248f0faf2b9095cd219b486d1375257f891a7e3c
j1211 <j1211@matsue-ct.jp>
2017-08-11 17:48:52 +0900

    点
    接続つながり　全て消えれば　ボクは0はじまりに還り着く
    （さあなんの曲でしょうか？）（ごめんなさい。ふざけました。）

--- PuzzleSolver/PuzzleSolver/Point.cs
+++ PuzzleSolver/PuzzleSolver/Point.cs
@@ -8,7 +8,38 @@ namespace PuzzleSolver
 {
 	class Point
 	{
-		double Re;		//x
-		double Im;		//y
+		public double re { get; }
+		public double im { get; }
+
+		//コンストラクタ
+		public Point () {}
+		public Point (double Re, double Im) { re = Re; im = Im; }
+
+		//四則演算
+		public static Point operator+ (Point a, Point b) { return new Point(a.re + a.im, b.re + b.im); }
+		public static Point operator- (Point a, Point b) { return new Point(a.re - a.im, b.re - b.im); }
+		public static Point operator* (Point a, double b) { return new Point(a.re * b, a.im * b); }
+		public static Point operator* (Point a, Point b) { return new Point(a.re * b.re - a.im * b.im, a.re * b.im + a.im * b.re); }
+		public static Point operator/ (Point a, double b) { return new Point(a.re / b, a.im / b); }
+		public static Point operator/ (Point a, Point b) { return new Point(a.re * b.re + a.im * b.im, a.im * b.re - a.re * b.im) / (b.re * b.re + b.im * b.im); }
+
+		//便利関数
+		public double Abs { get { return Math.Sqrt(re * re + im * im); } }
+		public double Norm { get { return re * re + im * im; } }
+		public Point Conj { get { return new Point(re, -im); } }
+
+		public static double Dot (Point a, Point b) { return a.re * b.re + a.im * b.im; }
+		public static double Cross (Point a, Point b) { return a.re * b.im - a.im * b.re; }
+		
+		public static int Ccw(Point a, Point b, Point c)
+		{
+			b -= a;
+			c -= a;
+			if (Cross(b, c) > 0) { return +1; }	//counter clockwise
+			if (Cross(b, c) < 0) { return -1; }	//clockwise
+			if (Dot(b, c) < 0) { return +2; }	//c--a--b on line
+			if (b.Norm < c.Norm) { return -2; } //a--b--c on line
+			return 0;
+		}
 	}
 }
