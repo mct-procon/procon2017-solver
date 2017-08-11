bde594f82d26ed22c061a5eb3c1e6cc21144f481
j1211 <j1211@matsue-ct.jp>
2017-08-11 23:42:39 +0900

    Swap関数の実装
    一般的な関数を実装します。

---
+++ PuzzleSolver/PuzzleSolver/Generic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolver
+{
+	static class Generic
+	{
+		public static void Swap<T>(ref T a, ref T b)
+		{
+			T t = a;
+			a = b;
+			b = t;
+		}
+	}
+}
