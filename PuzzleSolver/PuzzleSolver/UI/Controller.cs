73e4823f5de50f785d6d2941c47e3d845f353837
j1211 <j1211@matsue-ct.jp>
2017-08-12 16:52:04 +0900

    1問を解く
    動作テスト用

--- PuzzleSolver/PuzzleSolver/UI/Controller.cs
+++ PuzzleSolver/PuzzleSolver/UI/Controller.cs
@@ -5,20 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using DxLib;
 using PuzzleSolver.Core;
+using PuzzleSolver.Geometry;
 
 namespace PuzzleSolver.UI
 {
 	public class Controller
 	{
-		public Backup History;	//参照. パズルをStackで管理.
-		public View View;		//実体. 描画.
-		public Solve Solver;	//実体. 回答.
+		private Backup History;	//参照. パズルをStackで管理.
+		private View view;		//実体. 描画.
+		private Solve solve;	//実体. 回答.
 
-		Controller(Backup backup)
+		//コンストラクタ
+		public Controller() { }
+		public Controller(Backup backup, Point t, double scale, int windowSizeX, int windowSizeY)
 		{
 			this.History	= backup;
-			View	    	= new View();
-			Solver  		= new Solve();
+			view	    	= new View(t, scale, windowSizeX, windowSizeY);
+			solve  			= new Solve();
+		}
+
+		//問題を解く
+		public void Solve()
+		{
+			while (DX.ScreenFlip() == 0 && DX.ProcessMessage() == 0 && DX.ClearDrawScreen() == 0 && !DX.CheckHitKey(DX.KeyInput.Escape))
+			{
+				view.Update();
+				view.Draw(History.Peek());
+			}
 		}
 	}
 }
