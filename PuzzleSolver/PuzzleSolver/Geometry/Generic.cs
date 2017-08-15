b9b4be04847329690b63e0149cdc55bbe9c2f780
j1211 <j1211@matsue-ct.jp>
2017-08-15 15:54:24 +0900

    スワップ関数の実装
    マージ処理で必要になった「点と多角形の包含関係」で必要になったスワップ

---
+++ PuzzleSolver/PuzzleSolver/Geometry/Generic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolver.Geometry
+{
+	class Generic
+	{
+		public static void Swap<T>(ref T a, ref T b)
+		{
+			var t = a;
+			a = b;
+			b = t;
+		}
+	}
+}
