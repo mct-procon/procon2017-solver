47864f287f35ffdddfb1c55dcce8c008ba486845
j1618 <j1618@matsue-ct.jp>
2017-08-16 11:55:45 +0900

    Make getting key inputs better in Controller.cs.

--- PuzzleSolver/PuzzleSolver/UI/Controller.cs
+++ PuzzleSolver/PuzzleSolver/UI/Controller.cs
@@ -9,55 +9,56 @@ using PuzzleSolver.Geometry;
 
 namespace PuzzleSolver.UI
 {
-	public class Controller
-	{
-		private Backup History;	//参照. パズルをStackで管理.
-		private View view;		//実体. 描画.
-		private Solve solve;	//実体. 回答.
+    public class Controller
+    {
+        private Backup History;	//参照. パズルをStackで管理.
+        private View view;		//実体. 描画.
+        private Solve solve;	//実体. 回答.
 
-		//コンストラクタ
-		public Controller() { }
-		public Controller(Backup backup, Point t, double scale, int windowSizeX, int windowSizeY)
-		{
-			this.History	= backup;
-			view	    	= new View(t, scale, windowSizeX, windowSizeY);
-			solve  			= new Solve();
-		}
+        //コンストラクタ
+        public Controller() { }
+        public Controller(Backup backup, Point t, double scale, int windowSizeX, int windowSizeY)
+        {
+            this.History	= backup;
+            view	    	= new View(t, scale, windowSizeX, windowSizeY);
+            solve  			= new Solve();
+        }
 
-		//問題を解く
-		public void Solve()
-		{
-			bool[] bkey = new bool[256];
-			bool[] key = new bool[256];
-			for (int i = 0; i < 256; i++) { key[i] = false; }
+        //問題を解く
+        public void Solve()
+        {
+            (DX.KeyState state, DX.Result) prev_key;
+            (DX.KeyState state, DX.Result) key;
 
-			while (true)
-			{
-				bool breakFlag = false;
-				while (DX.ScreenFlip() == 0 && DX.ProcessMessage() == 0 && DX.ClearDrawScreen() == 0 && !DX.CheckHitKey(DX.KeyInput.Escape))
-				{
-					for (int i = 0; i < 256; i++) { bkey[i] = key[i]; }
-					for (int i = 0; i < 256; i++) { key[i] = DX.CheckHitKey((DX.KeyInput)i); }
+            key = DX.GetHitKeyStateAll();
 
-					if (!bkey[(int)DX.KeyInput.NumPadEnter] && key[(int)DX.KeyInput.NumPadEnter])
-					{
-						breakFlag = true;
-						break;
-					}
-					if (!bkey[(int)DX.KeyInput.Back] && key[(int)DX.KeyInput.Back])
-					{
-						History.Pop();
-					}
+            while (true)
+            {
+                bool breakFlag = false;
+                while (DX.ScreenFlip() == 0 && DX.ProcessMessage() == 0 && DX.ClearDrawScreen() == 0 && !DX.CheckHitKey(DX.KeyInput.Escape))
+                {
+                    prev_key = key;
+                    key = DX.GetHitKeyStateAll();
 
-					view.Update();
-					view.Draw(History.Peek());
-				}
-				if (!breakFlag) { return; }
+                    if (!prev_key.state[DX.KeyInput.NumPadEnter] && key.state[DX.KeyInput.NumPadEnter])
+                    {
+                        breakFlag = true;
+                        break;
+                    }
+                    if (prev_key.state[DX.KeyInput.Back] && key.state[DX.KeyInput.Back])
+                    {
+                        History.Pop();
+                    }
 
-				Puzzle puzzle = History.Peek().Clone();
-				puzzle = solve.ConnectAuto(puzzle);
-				History.Push(puzzle);
-			}
-		}
-	}
+                    view.Update();
+                    view.Draw(History.Peek());
+                }
+                if (!breakFlag) { return; }
+
+                Puzzle puzzle = History.Peek().Clone();
+                puzzle = solve.ConnectAuto(puzzle);
+                History.Push(puzzle);
+            }
+        }
+    }
 }
