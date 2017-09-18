b1ab638473c796e069e2f152e356bde8234dec7c
j1618 <j1618@matsue-ct.jp>
2017-09-18 22:11:42 +0900

    Added some members on Network.ProconPuzzleService. (Updated Procon2017MCTProtocol.dll)

--- PuzzleSolver/PuzzleSolver/Network/Networking.cs
+++ PuzzleSolver/PuzzleSolver/Network/Networking.cs
@@ -21,12 +21,53 @@ namespace PuzzleSolver.Network {
     }
 
     public class ProconPuzzleService : IProconPuzzleService {
+        public static volatile bool IsPolygonReceived = false;
+        static SendablePolygon _Polygon;
+        public static SendablePolygon Polygon {
+            get {
+                IsPolygonReceived = false;
+                SendablePolygon result;
+                lock(_Polygon) {
+                    result = _Polygon;
+                }
+                return result;
+            }
+        }
+
+        public static volatile bool IsQrCodeReceived = false;
+        static QRCodeData _QrCode;
+        public static QRCodeData QrCode {
+            get {
+                IsQrCodeReceived = false;
+                QRCodeData result;
+                lock (_QrCode) {
+                    result = _QrCode;
+                }
+                return result;
+            }
+        }
+
+
         void IProconPuzzleService.Polygon(SendablePolygon poly) {
-            // ...
+            if(_Polygon == null) 
+                _Polygon = poly;
+            else {
+                lock(_Polygon) {
+                    _Polygon = poly;
+                }
+            }
+            IsPolygonReceived = true;
         }
 
         void IProconPuzzleService.QRCode(QRCodeData data) {
-            // ...
+            if(_QrCode == null)
+                _QrCode = data;
+            else {
+                lock (_QrCode) {
+                    _QrCode = data;
+                }
+            }
+            IsQrCodeReceived = true;
         }
     }
 }
