999363989cd762b04160f1d9ff2ac84e905781c2
j1211 <keimahatokusyu@gmail.com>
2017-09-29 17:31:49 +0900

    支援システムについてコメントを書いた
    Webカメラに写った多角形のデータを
    取得する方法がわからないので、教えて
    ください！！

--- PuzzleSolver/PuzzleSolver/UI/Controller.cs
+++ PuzzleSolver/PuzzleSolver/UI/Controller.cs
@@ -39,6 +39,8 @@ namespace PuzzleSolver.UI
 			for (int i = 0; i < 100; i++) { States.Add(new SkewHeap()); }
 			States[0].Push(initialPuzzle);
 
+			int LoopCount = 0;
+
 			while (true)
             {
                 bool breakFlag = false;
@@ -67,7 +69,13 @@ namespace PuzzleSolver.UI
                     view.UpdateDrawInfo();
 
 					//表示したいパズルを渡す
-                    view.Draw(States[nowDepth].MaxValue());
+					Puzzle ViewPuzzle = States[nowDepth].MaxValue();
+					if (LoopCount % 10 == 0)
+					{
+						//支援システムから、Webカメラに写った多角形を読み取る。もし読み取れて、かつ、該当する多角形が存在すればViewクラスで表示。
+					}
+					LoopCount++;			//上記の処理のみで使っているので、ここにカウントアップを書いた。
+					view.Draw(ViewPuzzle);
 
 					//デバッグとして、ビームサーチの評価値を表示してみよう。
 					SkewHeap hoge = States[nowDepth].CloneShallow();
