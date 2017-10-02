85f0073113504fccef1e40ec456280ffae188566
j1211 <keimahatokusyu@gmail.com>
2017-10-02 17:14:03 +0900

    強調表示の実装（プレテスト済み）

--- PuzzleSolver/PuzzleSolver/UI/Controller.cs
+++ PuzzleSolver/PuzzleSolver/UI/Controller.cs
@@ -79,14 +79,14 @@ namespace PuzzleSolver.UI
 						if (id != -1) { strongDrawPieceId = id; }
 					}
 
+					//パズルの表示
+					view.Draw(ViewPuzzle);
+
 					//強調表示
 					if (strongDrawPieceId != -1)
 					{
-
+						view.DrawPieceStrong(ViewPuzzle, strongDrawPieceId);
 					}
-
-					//パズルの表示
-					view.Draw(ViewPuzzle);
 
 					//デバッグとして、ビームサーチの評価値を表示してみよう。
 					SkewHeap hoge = States[nowDepth].CloneShallow();
@@ -119,11 +119,144 @@ namespace PuzzleSolver.UI
             }
         }
 
+
 		//引数：表示するPuzzle, 支援システムから受け取った多角形
 		//戻り値：強調表示すべきピースの番号. 該当するものがなければ-1を返す.
 		private int CalcPieceId(Puzzle puzzle, Poly piece)
 		{
-			return -1;
+			List<List<Point>> initPieces = new List<List<Point>>();
+			int i, j;
+			
+			for (i = 0; i < puzzle.initPieceNum; i++)
+			{
+				initPieces.Add(new List<Point>());
+			}
+
+			for (i = 0; i < puzzle.wakuLines.Count; i++)
+			{
+				Line line = puzzle.wakuLines[i];
+				if (line.initPieceId >= 0)
+				{
+					initPieces[line.initPieceId].Add(line.start);
+				}
+			}
+
+			for (i = 0; i < puzzle.pieces.Count; i++)
+			{
+				if (!puzzle.pieces[i].isExist)
+				{
+					continue;
+				}
+				for (j = 0; j < puzzle.pieces[i].lines.Count; j++)
+				{
+					Line line = puzzle.pieces[i].lines[j];
+					if (line.initPieceId >= 0)
+					{
+						initPieces[line.initPieceId].Add(line.start);
+					}
+				}
+			}
+
+			double minA = 11451419;
+			int ret = -1;
+			for (i = 0; i < puzzle.initPieceNum; i++)
+			{
+				initPieces[i].Add(initPieces[i][0]);
+				Poly initPiece = new Poly(initPieces[i], new List<Line>(), true);
+				double a = EvalLogScale(initPiece, piece.Clone());
+				if (minA > a)
+				{
+					minA = a;
+					ret = i;
+				}
+			}
+			return ret;
+		}
+
+
+		//なんかあれ
+		private double EvalLogScale(Poly poly1, Poly poly2)
+		{
+			int i, j;
+			double ret = 1145141919;
+			double errorDeg = 10;
+
+			poly1 = RemovePoints(poly1, errorDeg);
+			poly2 = RemovePoints(poly2, errorDeg);
+			if (poly1.Count != poly2.Count) { return 1145141919; }
+			Scaling(poly1);
+			Scaling(poly2);
+
+			int n = poly1.Count;
+			for (i = 0; i < n; i++)	//poly1[0], poly2[i]から走査
+			{
+				double maxLogScale = 0;
+				for (j = 0; j < n; j++)
+				{
+					Point a = poly1[j + 1] - poly1[j];
+					Point b = poly2[i + j + 1] - poly2[i + j];
+
+					if (a.Abs < 1e-9 || b.Abs < 1e-9)
+					{
+						return 1145141919;
+					}
+					maxLogScale = Math.Max(maxLogScale, Math.Abs(Math.Log(a.Abs / b.Abs)));
+				}
+				ret = Math.Min(ret, maxLogScale);
+			}
+			return ret;
+		}
+
+		///角度が180度に近い頂点を取り除く
+		private Poly RemovePoints(Poly poly, double errorDegree)
+		{
+			int i;
+			const double PAI = 3.14159265358979;
+			Poly ret = new Poly(new List<Point>(), new List<Line>(), true);
+
+			for (i = 0; i < poly.Count; i++)
+			{
+				Point a = poly[i - 1];
+				Point b = poly[i];
+				Point c = poly[i + 1];
+
+				Point ba = a - b;
+				Point cb = c - b;
+				Point hoge = (ba / cb);
+
+				double deg = Math.Atan2(hoge.Im, hoge.Re) * 180 / PAI;
+				if (deg < 0) { deg += 360; }
+
+				if (Math.Abs(deg - 180) <= errorDegree)
+				{
+					continue;
+				}
+
+				ret.points.Add(b);
+			}
+			ret.points.Add(ret.points[0]);
+
+			return ret;
+		}
+
+		//正規化 (最も短い辺の長さが1になるように拡大（縮小）する）
+		private void Scaling(Poly poly)
+		{
+			double minLength = 1145141919;
+			int i;
+
+			poly.Trans(poly.points[0], false);
+
+			for (i = 0; i < poly.Count; i++)
+			{
+				minLength = Math.Min(minLength, (poly.points[i + 1] - poly.points[i]).Abs);
+			}
+
+			//minLength倍縮小する。
+			for (i = 0; i < poly.Count; i++)
+			{
+				poly[i] /= minLength;
+			}
 		}
     }
 }
