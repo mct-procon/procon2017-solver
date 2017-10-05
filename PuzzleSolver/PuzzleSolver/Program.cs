47661fb0376a6642628af0ba8ef7952e91fe3380
j1211 <j1211@users.noreply.github.com>
2017-10-05 20:40:16 +0900

    大きく息を吸って～背筋を伸ばす運動から！（画面サイズを可変にするなどをした）

--- PuzzleSolver/PuzzleSolver/Program.cs
+++ PuzzleSolver/PuzzleSolver/Program.cs
@@ -12,94 +12,108 @@ using Microsoft.Win32;
 
 namespace PuzzleSolver
 {
-    /// <summary>
-    /// メイン
-    /// </summary>
-    class Program
-    {
-        static Read read;					//実体. ファイル読み込み
-        static Controller	controller;		//実体. 1問解く／表示する
+	/// <summary>
+	/// メイン
+	/// </summary>
+	class Program
+	{
+		static Read read;                   //実体. ファイル読み込み
+		static Controller controller;       //実体. 1問解く／表示する
 
-        static IntPtr MainWindowHWND;       //DxLibが作ったウィンドウのハンドル
-        static NativeWindow MainWindow;     //DxLibが作ったウィンドウ（MessageBox表示用）
-        static Network.WCF WCFServer;       //支援システムとの通信を管理するクラス．
+		static IntPtr MainWindowHWND;       //DxLibが作ったウィンドウのハンドル
+		static NativeWindow MainWindow;     //DxLibが作ったウィンドウ（MessageBox表示用）
+		static Network.WCF WCFServer;       //支援システムとの通信を管理するクラス．
 
-        /// <summary>
-        /// メイン
-        /// </summary>
-        /// <param name="args">引数</param>
-        [STAThread]
-        static void Main(string[] args)
-        {
-            DX.ChangeWindowMode(true);
-            DX.SetBackgroundColor(255, 255, 255);
-            DX.SetGraphMode(1400, 1000, 32);
-            if (DX.Init() == DX.Result.Error) Environment.Exit(-1);
-            DX.SetDrawScreen(DX.Screen.Back);
+		/// <summary>
+		/// メイン
+		/// </summary>
+		/// <param name="args">引数</param>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			DX.ChangeWindowMode(true);                        // Set Window Mode against FullScreen Mode.
+			DX.SetBackgroundColor(255, 255, 255);             // Set Background Color of Screen.
+			DX.SetWindowSizeChangeEnableFlag(1, false);    // Set Resizable and non-Scaling.
+			DX.SetWindowSize(1400, 1000);                       // Set Window Size
+			DX.SetAlwaysRunFlag(1);                        // Set to run even when the window is not active.
+			DX.SetGraphMode(1920, 1080, 32);                  // Set Main Render's Size.
+			if (DX.Init() == DX.Result.Error) Environment.Exit(-1);
+			DX.SetDrawScreen(DX.Screen.Back);
 
-            MainWindowHWND = DX.GetMainWindowHandle();
-            MainWindow = NativeWindow.FromHandle(MainWindowHWND);
+			MainWindowHWND = DX.GetMainWindowHandle();
+			MainWindow = NativeWindow.FromHandle(MainWindowHWND);
 
-            read = new Read();
-            controller = new Controller(new Point(0, 0), 5.0, 1400, 1000);
+			read = new Read();
+			controller = new Controller(new Point(0, 0), 5.0, 1400, 1000);
 
-            DX.ClsDx();
+			DX.ClsDx();
 
-            WCFServer = new Network.WCF();
-            try {
-                WCFServer.Open();
-            } catch(System.ServiceModel.AddressAccessDeniedException exp) {
+			WCFServer = new Network.WCF();
+			try
+			{
+				WCFServer.Open();
+			}
+			catch (System.ServiceModel.AddressAccessDeniedException exp)
+			{
 #if !DEBUG
-                MessageBox.Show(MainWindow, "HTTPサーバーの作成でアクセス拒否が起きました．netsh等で，権限付与してくださいな．", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				MessageBox.Show(MainWindow, "HTTPサーバーの作成でアクセス拒否が起きました．netsh等で，権限付与してくださいな．", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
 #endif
-            } catch(Exception ex) {
+			}
+			catch (Exception ex)
+			{
 #if !DEBUG
-                MessageBox.Show(MainWindow, $"エラーが起きました．\n{ex}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				MessageBox.Show(MainWindow, $"エラーが起きました．\n{ex}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
 #endif
-            }
+			}
 
-			//パズルを読む
 			Puzzle initialPuzzle;
-			//initialPuzzle= ReadFile(@"C:\Users\Hashimotolab\Documents\GitHub\procon2017-solver\PuzzleSolver\PuzzleSolver\TestCases\ProconSample\qrresult.txt");
-			while (!Network.ProconPuzzleService.IsQrCodeReceived) ;
-			initialPuzzle = read.ReadFromQRCode(Network.ProconPuzzleService.QrCode);
+			initialPuzzle = ReadFile(@"C:\Users\hashimotolab\Documents\GitHub\procon2017-solver\PuzzleSolver\PuzzleSolver\TestCases\ProconSample\qrresult.txt");
+			//while (!Network.ProconPuzzleService.IsQrCodeReceived) ;
+			//initialPuzzle = read.ReadFromQRCode(Network.ProconPuzzleService.QrCode);
 			if (initialPuzzle == null) { DX.Finalize(); return; }
 
 			controller.Syakunetsukun(initialPuzzle);
 
-            WCFServer.Close();
+			WCFServer.Close();
 
-            DX.Finalize();
-        }
+			DX.Finalize();
+		}
 
-        /// <summary>
-        /// 問題ファイルを読み込みます
-        /// </summary>
-        /// <param name="FilePath">読み込むファイルパス</param>
-        /// <returns>読み込んだPuzzleの参照先 (失敗時はnullを返す) </returns>
-        static Puzzle ReadFile(string FilePath) {
-            try {
-                return read.ReadFile(FilePath);
-            } catch (Exception ex) {
-                DX.WriteLineDx("File Input Error.\n{0}", ex);
-                DX.ScreenFlip();
-                if (MessageBox.Show(MainWindow, $"{FilePath}が読み込めませんでした．\n別のファイルを読み込みますか？", "エラー", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly) == DialogResult.Yes) {
-                    OpenFileDialog openFileDialog = new OpenFileDialog();
-                    openFileDialog.InitialDirectory = Environment.CurrentDirectory;
-                    openFileDialog.Filter = "All Files|*.*";
-                    openFileDialog.FilterIndex = 0;
-                    openFileDialog.Title = "問題ファイルをえらんでね";
-                    openFileDialog.Multiselect = false;
-                    if (openFileDialog.ShowDialog() == DialogResult.OK)
-                        return ReadFile(openFileDialog.FileName);
-                }
-                while (DX.ProcessMessage() == DX.Result.Success) {
-                    if (DX.GetMouseInput() == DX.MouseInput.Left)
-                        break;
-                }
-                DX.Finalize();
+		/// <summary>
+		/// 問題ファイルを読み込みます
+		/// </summary>
+		/// <param name="FilePath">読み込むファイルパス</param>
+		/// <returns>読み込んだPuzzleの参照先 (失敗時はnullを返す) </returns>
+		static Puzzle ReadFile(string FilePath)
+		{
+			try
+			{
+				read.ReadFile(FilePath);
+				return read.ReadFile(FilePath);
+			}
+			catch (Exception ex)
+			{
+				DX.WriteLineDx("File Input Error.\n{0}", ex);
+				DX.ScreenFlip();
+				if (MessageBox.Show(MainWindow, $"{FilePath}が読み込めませんでした．\n別のファイルを読み込みますか？", "エラー", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly) == DialogResult.Yes)
+				{
+					OpenFileDialog openFileDialog = new OpenFileDialog();
+					openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+					openFileDialog.Filter = "All Files|*.*";
+					openFileDialog.FilterIndex = 0;
+					openFileDialog.Title = "問題ファイルをえらんでね";
+					openFileDialog.Multiselect = false;
+					if (openFileDialog.ShowDialog() == DialogResult.OK)
+						return ReadFile(openFileDialog.FileName);
+				}
+				while (DX.ProcessMessage() == DX.Result.Success)
+				{
+					if (DX.GetMouseInput() == DX.MouseInput.Left)
+						break;
+				}
+				DX.Finalize();
 				return null;
-            }
-        }
-    }
+			}
+		}
+	}
 }
