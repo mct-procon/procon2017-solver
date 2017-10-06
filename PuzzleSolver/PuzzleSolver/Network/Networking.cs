07e90daae6d3c07e2a080e479cd7062098e1d17a
Go Suzuki <j1618@matsue-ct.jp>
2017-10-06 11:11:36 +0900

    Updated Networking. not tested yet! : )

--- PuzzleSolver/PuzzleSolver/Network/Networking.cs
+++ PuzzleSolver/PuzzleSolver/Network/Networking.cs
@@ -8,19 +8,31 @@ using Procon2017MCTProtocol;
 
 namespace PuzzleSolver.Network {
     public class WCF {
-        ServiceHost svc = new ServiceHost(typeof(ProconPuzzleService));
-        public void Open() {
-            svc.AddServiceEndpoint(typeof(IProconPuzzleService), new BasicHttpBinding(), Parameter.ProconPuzzUri);
-            svc.Open();
+        IProconPuzzleService proconService;
+        ProconPuzzleService Receiver;
+
+        public void StartWCFSender()
+        {
+            Receiver = new ProconPuzzleService();
+
+            BasicHttpBinding binding = new BasicHttpBinding();
+
+            DuplexChannelFactory<IProconPuzzleService> factory = new DuplexChannelFactory<IProconPuzzleService>(proconService);
+            factory.Endpoint.Binding = new BasicHttpBinding();
+            factory.Endpoint.Contract.ContractType = typeof(IProconPuzzleService);
+            factory.Endpoint.Address = new EndpointAddress(Parameter.ProconPuzzUri.AbsoluteUri);
+
+            proconService = factory.CreateChannel();
         }
 
-        public void Close() {
-            if(svc.State == CommunicationState.Opened)
-                svc.Close();
+        public class Dummy : IProconPuzzleService
+        {
+            public void Polygon(SendablePolygon poly) { }
+            public void QRCode(QRCodeData data) { }
         }
     }
 
-    public class ProconPuzzleService : IProconPuzzleService {
+    public class ProconPuzzleService : Procon2017MCTProtocol.IDuplexCallback {
         public static volatile bool IsPolygonReceived = false;
         static SendablePolygon _Polygon;
         public static SendablePolygon Polygon {
@@ -48,7 +60,7 @@ namespace PuzzleSolver.Network {
         }
 
 
-        void IProconPuzzleService.Polygon(SendablePolygon poly) {
+        void IDuplexCallback.Polygon(SendablePolygon poly) {
             if(_Polygon == null) 
                 _Polygon = poly;
             else {
@@ -59,7 +71,7 @@ namespace PuzzleSolver.Network {
             IsPolygonReceived = true;
         }
 
-        void IProconPuzzleService.QRCode(QRCodeData data) {
+        void IDuplexCallback.QRCode(QRCodeData data) {
             if(_QrCode == null)
                 _QrCode = data;
             else {
